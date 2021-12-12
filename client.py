cd6a89b2452396225ae66961349ead5eb9d9f0e3
MikeMarshall36 <90028680+MikeMarshall36@users.noreply.github.com>
2021-12-12 13:23:14 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,14 +1,105 @@
 import socket
-import pickle
+from threading import Thread
+from binascii import hexlify, unhexlify
+from itertools import cycle
+from time import sleep
 
-HOST = '127.0.0.1'
-PORT = 8080
+def incription(incript, key):
+    cipher = xor_str(incript, key)
+    return (hexlify(cipher.encode())).decode()
+
+def decription(incript, key):
+    incript = (unhexlify(incript.encode())).decode()
+    return xor_str(incript, key)
+
+def xor_str(a, b):
+    return ''.join([chr(ord(x) ^ ord(y)) for x, y in zip(a, cycle(b))])
+
+
+def generation(key_prim, key_publ_m):
+    msg = str(key_publ_m)
+    sleep(0.1)
+    sock.send(msg.encode('utf-8'))
+    try:
+        key_publ_s = int(sock.recv(1024))
+    except ValueError:
+        print("Error: KeyNotCorrect")
+        global flag
+        flag = False
+        return (False)
+    key_part_m = calc_key(key_publ_m, key_prim, key_publ_s)
+    msg = str(key_part_m)
+    sleep(0.1)
+    sock.send(msg.encode('utf-8'))
+    key_part_s = int(sock.recv(1024))
+    key_full_m = calc_key(key_part_s, key_prim, key_publ_s)
+    with open('key' + str(pr) + '.txt', 'w') as f:
+        f.write(str(key_full_m))
+    return key_full_m
+
+def calc_key(key_g, key_ab, key_p):
+    return key_g ** key_ab % key_p
+
+
+def recever():
+    global flag
+    while flag:
+        try:
+            inp = decription(sock.recv(1024).decode('utf-8'), key_full_s)
+            if inp == 'exit':
+                print('server out')
+                flag = False
+            else:
+                print('server:', inp)
+        except OSError:
+            flag = False
+
+flag = True
 
 sock = socket.socket()
-sock.connect((HOST, PORT))
+pr = 5170
+try:
+    sock.bind(('', pr))
+except OSError:
+    print('Addr error')
+    flag = False
+if flag:
+    sock.setblocking(1)
+    port_num = 53480
+    try:
+        sock.connect(('localhost', port_num))
+        print('connection with server')
+    except ConnectionRefusedError:
+        print('Server not online')
+        flag = False
+if flag:
+    try:
+        file = open('key' + str(pr) + '.txt', 'r')
+        key_full_s = file.read()
+        file.close()
+    except:
+        key_prim = 199
+        key_publ_m = 197
+        i = 0
+        key_full_s = str(generation(key_prim, key_publ_m))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
-
+if flag:
+    port = decription(sock.recv(1024).decode('utf-8'), key_full_s)
+    sock.close()
+if flag:
+    sock = socket.socket()
+    sock.bind(('', pr))
+    sock.setblocking(1)
+    sock.connect(('localhost', int(port)))
+    stream = Thread(target=recever)
+    stream.start()
+    while flag:
+        msg = input('input your message: ')
+        if msg == 'exit':
+            flag = False
+        try:
+            msg = incription(msg, key_full_s)
+            sock.send(msg.encode('utf-8'))
+        except ConnectionResetError:
+            flag = False
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,129 @@
+from contextlib import closing
+from threading import Thread
 import socket
-import pickle
+import csv
+from binascii import hexlify, unhexlify
+from itertools import cycle
+from time import sleep
 
-HOST = '127.0.0.1'
-PORT = 8080
+def encodeing(incription, key):
+    cipher = xor_str(incription, key)
+    return (hexlify(cipher.encode())).decode()
 
+def decoding(incription, key):
+    incription = (unhexlify(incription.encode())).decode()
+    return xor_str(incription, key)
+
+def xor_str(a, b):
+    return ''.join([chr(ord(x) ^ ord(y)) for x, y in zip(a, cycle(b))])
+
+def openport_detector():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        sock.bind(('', 0))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return sock.getsockname()[1]
+
+def generation(key_prim, key_publ_m):
+    key_publ_s = int(conn.recv(1024))
+    if check(key_publ_s):
+        msg = str(key_publ_m)
+        sleep(0.1)
+        conn.send(msg.encode('utf-8'))
+    else:
+        print("Error: KeyNotCorrect")
+        global flag
+        flag = False
+        return (False)
+
+    key_part_s = int(conn.recv(1024))
+    key_part_m = calc_key(key_publ_s, key_prim, key_publ_m)
+    msg = str(key_part_m)
+    sleep(0.1)
+    conn.send(msg.encode('utf-8'))
+
+    key_full_m = calc_key(key_part_s, key_prim, key_publ_m)
+    with open('server_key' + str(addr) + '.txt', 'w') as f:
+        f.write(str(key_full_m))
+    return key_full_m
+
+def calc_key(key_g, key_ab, key_p):
+    return key_g ** key_ab % key_p
+
+def reciver():
+    global flag
+    while flag:
+        try:
+            inp = decoding(conn.recv(1024).decode('utf-8'), key_full_m)
+            if inp == 'exit':
+                print('client out')
+                flag = False
+            else:
+                print('client:', inp)
+        except OSError:
+            flag = False
+
+def check(key_publ_s):
+    i = False
+    with open('key_list.csv', 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            if line[0] == str(key_publ_s):
+                i = True
+    return i
+
+flag = True
 sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+num = 53480
+print('Your port:', num)
+try:
+    sock.bind(('', num))
+except OSError:
+    num = openport_detector()
+    print('Ошибка. Выбранный вами код сервера уже занят, код сервера будет изменён автоматически. Новый код: ', num)
+    sock.bind(('', num))
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+print('Server activate')
+sock.listen(3)
+i = 0
+conn, addr = sock.accept()
+try:
+    file = open('keyserv' + str(addr) + '.txt', 'r')
+    key_full_m = file.read()
+    file.close()
+except:
+    key_publ_m = 151
+    key_prim = 157
+
+    key_full_m = str(generation(key_prim, key_publ_m))
+
+if flag:
+    print('user addr:', addr)
+    port = openport_detector()
+    msg = encodeing(str(port), key_full_m)
+    conn.send(msg.encode('utf-8'))
+
+    conn.close()
+if flag:
+    sock = socket.socket()
+    sock.bind(('', int(port)))
+    sock.listen(3)
+    addr2 = ''
+    while True:
+        conn, addr2 = sock.accept()
+        if addr2 == addr:
+            break
+        else:
+            conn.close()
+    stream = Thread(target=reciver())
+    stream.start()
+    while flag:
+        msg = input('input your message: ')
+        if msg == 'exit':
+            flag = False
+        try:
+            msg = encodeing(msg, key_full_m)
+            conn.send(msg.encode('utf-8'))
+        except ConnectionResetError:
+            flag = False
 
 conn.close()
